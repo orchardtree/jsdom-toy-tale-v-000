0f916f56868b3a3a78e95ee12143ff40c83140f7
Jesse Wenger <wengerj@gmail.com>
2021-06-11 11:59:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,3 +13,82 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+/* //CARD
+<div class="card">
+  <h2>Woody</h2>
+  <img src=toy_image_url class="toy-avatar" />
+  <p>4 Likes </p>
+  <button class="like-btn">Like <3</button>
+</div>
+*/
+
+/*
+headers:
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+}
+
+body: JSON.stringify({
+  "name": "Jessie",
+  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  "likes": 0
+})
+*/
+
+/*
+function submitData(newName, newEmail) {
+  const userUrl = "http://localhost:3000/users"
+
+  const formData = {
+    name: newName,
+    email: newEmail
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch(userUrl, configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+    renderUsers(object);
+  })
+  .catch(function(error) {
+    console.log(error.message);
+    renderErrors(error)
+  });
+}
+
+function renderUsers(newUser) {
+  const newUserId = newUser.id;
+  renderListItem(newUserId)
+}
+
+function renderErrors(error) {
+  const errorMsg = error.message
+  renderListItem(errorMsg)
+}
+
+function renderListItem (item) {
+  if (!document.getElementById('userIds')) {
+    const newUl = document.createElement('ul');
+    document.body.appendChild(newUl).id = "userIds";
+  }
+  const ul = document.getElementById('userIds');
+  const li = document.createElement('li');
+  li.innerHTML = item;
+  ul.appendChild(li);
+  console.log(item);
+}
+*/
+ 
